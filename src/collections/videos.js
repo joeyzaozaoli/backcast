259ec7f5a0863c8f3720ee833637fa3b0c2b72fc
Joey Li <joeyzaozaoli@gmail.com>
2017-09-03 14:30:01 -0700

    Build #search function to retrieve live data from YouTube

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,34 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+
+  search: function(query) {
+    Backbone.ajax({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      type: 'GET',
+      data: {
+        part: 'snippet',
+        maxResults: 5,
+        q: query,
+        type: 'video',
+        videoEmbeddable: 'true',
+        key: window.YOUTUBE_API_KEY
+      },
+
+      success: function(results) {
+        console.log('get success', results);
+        this.parse(results);
+      },
+
+      error: function(error) {
+        console.log('get failure', error);
+      }
+    });
+  },
+
+  parse: function(results) {
+    return results.items;
+  }
 
 });
+
